838cf98a49252a7dd4e61d05af71f1c7e2b209d4
Steven Wong <syw14@sfu.ca>
2022-12-29 19:19:50 -0800

    initial attempt at problem

---
+++ stack/20.valid-parentheses/solution.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(char a, char b)
+    {
+        if (a == '{' && b == '}') return true;
+        if (a == '[' && b == ']') return true;
+        if (a == '(' && b == ')') return true;
+        return false;
+    }
+
+    bool isValid(string s) {
+        stack<char> stk;
+        // Iterate through our string and push/pop elements from our stack
+        for (auto c : s)
+        {
+            // If we have an open parentheses, push it onto our stack
+            if (c == '{' || c == '[' || c == '(')
+                stk.push(c);
+            // Otherwise, check if we have a matching closing parentheses
+            else
+            {
+                // If stack is empty, invalid
+                if (stk.empty()) return false;
+                if (!isMatch(stk.top(), c)) return false;
+                stk.pop();
+            }
+
+        }
+        // At this point check if the stack is empty
+        return stk.empty();
+    }
+};
