a2852ac178a64a1967d7597c4e202de654f09bad
Steven Wong <syw14@sfu.ca>
2022-12-30 00:01:02 -0800

    update solution to complete in one func

--- stack/20.valid-parentheses/solution.cpp
+++ stack/20.valid-parentheses/solution.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 class Solution {
 public:
-    bool isMatch(char a, char b)
-    {
-        if (a == '{' && b == '}') return true;
-        if (a == '[' && b == ']') return true;
-        if (a == '(' && b == ')') return true;
-        return false;
-    }
+    // bool isMatch(char a, char b)
+    // {
+    //     if (a == '{' && b == '}') return true;
+    //     if (a == '[' && b == ']') return true;
+    //     if (a == '(' && b == ')') return true;
+    //     return false;
+    // }
 
     bool isValid(string s) {
         stack<char> stk;
@@ -27,10 +27,12 @@ public:
             {
                 // If stack is empty, invalid
                 if (stk.empty()) return false;
-                if (!isMatch(stk.top(), c)) return false;
+                if (c == '}' && stk.top() != '{') return false;
+                if (c == ']' && stk.top() != '[') return false;
+                if (c == ')' && stk.top() != '(') return false;
+                // if (!isMatch(stk.top(), c)) return false;
                 stk.pop();
             }
-
         }
         // At this point check if the stack is empty
         return stk.empty();
